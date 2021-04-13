acc95032a45c38d38e03b18d951740b18095fb9e
HaeAna <haeana@csu.fullerton.edu>
2021-04-12 21:21:37 -0700

    Start of main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(){
+	char answer;
+	cout << "Welcome to the Word Association Game database Porject!!" << endl;
+	cout << "Would you like to clear your database?" << endl;
+	cin >> answer;
+
+	return 0;
+}
