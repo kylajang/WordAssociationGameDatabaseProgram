c7adb4c8c4248ed25a69f23af43a41466c242044
Stephan Rossel <stephanrossel@csu.fullerton.edu>
2021-04-14 16:41:44 -0700

    changes to the main.cpp file for loop allowing users to enter database entries

--- main.cpp
+++ main.cpp
@@ -10,5 +10,23 @@ int main(){
 	cout << "Would you like to clear your database?" << endl;
 	cin >> answer;
 
+	//Here the program begins accepting new entries to the database
+	
+	/* Ana, I put these variable names for now, I can change them to what
+	you name them in your section or you can use these names if you want */
+	
+	int current_word_position;
+	int total_words_added;
+	
+	//This while loop will allow the user to enter their words until done
+	while (current_word_position <= total_words_added)
+	{
+		cout << "Now creating new database entry "
+			<< current_word_position << " of "
+			<< total_words_added;
+	
+	}
+	
+	
 	return 0;
 }
